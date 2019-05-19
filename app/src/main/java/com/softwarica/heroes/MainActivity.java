6936a398c2d473158e2bb5946a9f75a8219a2abb
shiva <shivamaharjan1234567890@gmail.com>
2019-05-19 21:38:31 +0545

    added image feature

--- app/src/main/java/com/softwarica/heroes/MainActivity.java
+++ app/src/main/java/com/softwarica/heroes/MainActivity.java
@@ -1,14 +1,24 @@
 package com.softwarica.heroes;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +36,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText etName, etDesc;
     private Button btnAdd, btnByField, btnByMap, btnGet;
     private TextView tvResult;
+    private ImageView ivImage;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private String imagePath;
 
 
     @Override
@@ -40,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         tvResult = findViewById(R.id.tvResult);
         btnByField = findViewById(R.id.btnAddByField);
         btnByMap = findViewById(R.id.btnAddByMap);
+        ivImage = findViewById(R.id.ivImage);
+        swipeRefreshLayout = findViewById(R.id.swipe);
 
 
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -49,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ivImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+        });
 
         btnByMap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +96,45 @@ public class MainActivity extends AppCompatActivity {
                 showData();
             }
         });
+    }
+
+    private void BrowseImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(data == null){
+                Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Uri uri = data.getData();
+        imagePath = getRealPathFromUri(uri);
+        previewImage(imagePath);
+    }
+
+
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection,null,null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
+    }
+    private void previewImage(String ivImag) {
+        File imgFile = new File(ivImag);
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            ivImage.setImageBitmap(myBitmap);
+        }
     }
 
     private void saveDataByField() {
