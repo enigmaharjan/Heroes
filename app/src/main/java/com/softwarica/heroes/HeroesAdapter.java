532be40b304b3d350276f4a1ac1c0240d16d9529
shiva <shivamaharjan1234567890@gmail.com>
2019-05-21 08:28:19 +0545

    done

--- app/src/main/java/com/softwarica/heroes/HeroesAdapter.java
+++ app/src/main/java/com/softwarica/heroes/HeroesAdapter.java
@@ -1,6 +1,8 @@
 package com.softwarica.heroes;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,9 +11,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
 import model.Heroes;
+import model.url;
 
 class HeroesAdapter extends RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder> {
     Context mContext;
@@ -36,7 +42,15 @@ class HeroesAdapter extends RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder>
         final Heroes heroes = heroesList.get(i);
 //        heroesViewHolder.image.setImage(heroes.getImage());
         String img = heroes.getImage();
+        String imgpath = url.BASE_URL + "uploads/"+ heroes.getImage();
+        StrictMode();
 
+        try{
+            URL url = new URL(imgpath);
+            heroesViewHolder.image.setImageBitmap(BitmapFactory.decodeStream((InputStream) url.getContent()));
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
         heroesViewHolder.tvName.setText(heroes.getName());
         heroesViewHolder.tvDesc.setText(heroes.getDesc());
 
@@ -62,5 +76,9 @@ class HeroesAdapter extends RecyclerView.Adapter<HeroesAdapter.HeroesViewHolder>
             tvDesc = itemView.findViewById(R.id.tvDesc);
 
         }
+    }
+    private void StrictMode(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 }
